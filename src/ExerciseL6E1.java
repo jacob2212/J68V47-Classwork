73954ad60a0b69567f37ecff9bbfbd27ab993bb7
jacob2212 <30129084@nescol.ac.uk>
2023-10-04 10:30:17 +0100

    work

---
+++ src/ExerciseL6E1.java
@@ -0,0 +1,13 @@
+public class ExerciseL6E1 {
+    public static void main(String[] args) {
+
+        int number = 10;
+
+        while (number <= 24) {
+            if (number % 2 == 0) {
+                System.out.println(number);
+            }
+            number++;
+        }
+    }
+}
